5bf9653a5aa430b7f386be76b59f832bfe92a2c9
Andy Cohen <outlawandy@gmail.com>
2024-08-14 13:31:28 -0500

    Initial commit

---
+++ lib/chromium/pdf.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative "pdf/version"
+
+module Chromium
+  module Pdf
+
+    ##
+    # @param unescaped_filename [String] The filename to save the PDF as.
+    # @param print_url [String] The URL of the page you want to be processed.
+    # @param arguments [Array<String>] An array of arguments to pass to the Chrome binary.
+    # @yield [file, filename] Yields the file object and the filename to the block.
+    #
+    def generate_pdf!(unescaped_filename, print_url, arguments: ['--headless --disable-gpu'])
+      chrome_path = ENV.fetch('GOOGLE_CHROME_BIN', nil)
+      filename = unescaped_filename.gsub('&', 'and')
+
+      Dir.mktmpdir do |path|
+        filepath = "#{path}/#{filename}"
+
+        system("LD_PRELOAD='' #{chrome_path} --print-to-pdf='#{filepath}' #{arguments.join(' ')} #{print_url} 2> /dev/null"
+
+        File.open(filepath) do |file|
+          yield file, filename
+        end
+      end
+    end
+  end
+end
---
+++ lib/chromium/pdf/generators/chromium/pdf/install/install_generator.rb
@@ -0,0 +1,12 @@
+require 'rails/generators/base'
+require 'rails/generators/active_record'
+
+module Chromium
+  module Pdf
+    module Generators
+      class InstallGenerator < Rails::Generators::Base
+
+      end
+    end
+  end
+end
