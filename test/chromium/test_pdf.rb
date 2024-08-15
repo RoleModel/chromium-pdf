9aa1d1d4007dd5a9d82520c37bc28fdcfd3168b0
Blaine Irvin <blaine.r.irvin@gmail.com>
2024-08-15 13:15:42 -0500

    Test Pdf Concern

--- test/chromium/test_pdf.rb
+++ test/chromium/test_pdf.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'test/jobs/test_generate_pdf_job'
 
 class Chromium::TestPdf < Minitest::Test
-  # def test_that_it_has_a_version_number
-  #   refute_nil ::Chromium::Pdf::VERSION
-  # end
+  def test_job_includes_pdf_concern
+    assert_respond_to TestGeneratePdfJob.new, :generate_pdf!
+  end
 
-  # def test_it_does_something_useful
-  #   assert false
-  # end
+  def test_generate_pdf_calls_chrome_print
+    job = TestGeneratePdfJob.new
+    job.stub :chrome_print, :ran do
+      assert_equal :ran, job.generate_pdf!('filename', 'url')
+    end
+  end
+
+  def test_chrome_print_calls_file_open
+    job = TestGeneratePdfJob.new
+    File.stub :open, :ran do
+      result = job.send(:chrome_print, 'chrome', 'url', 'name', 'path', ['argument']) do |_file, filename|
+        assert_equal 'name', filename
+      end
+      assert_equal :ran, result
+    end
+  end
 end
